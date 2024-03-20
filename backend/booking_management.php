e9b0b5a970108cc589a037fc8043179e529a40ef
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-20 23:26:29 +0200

    Booking Management PHP+HTML

---
+++ backend/booking_management.php
@@ -0,0 +1,32 @@
+<?php
+
+include('connection.php');
+
+$query = $mysqli ->prepare('SELECT * FROM bookings');
+
+$query ->execute();
+
+$query ->store_result();
+
+$query ->bind_result($BookingID, $UserID, $FlightID, $BookingDate, $NumberofPassenger, $SeatNumbers, $PaymentStatus);
+
+$response = [];
+
+while ($query ->fetch()) {
+    $news = [
+        'BookingID' => $BookingID,
+        'UserID' => $UserID,
+        'FlightID' => $FlightID,
+        'BookingDate' => $BookingDate,
+        'NumberofPassenger' => $NumberofPassenger,
+        'SeatNumbers' => $SeatNumbers,
+        'PaymentStatus' => $PaymentStatus
+    ];
+    $response[] = $news;
+}
+
+$update_booking = $mysqli->query('UPDATE bookings SET flightID = ?, BookingDate = ?, NumberofPassenger = ?, SeatNumbers = ?, PaymentStatus = ? WHERE UserID = ?'); 
+
+echo json_encode($response);
+
+$mysqli ->close();
