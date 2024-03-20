f90b8425a90f002d7a2680d2fcde9d55e6b48d60
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-20 22:40:22 +0200

    fixed message.html

--- frontend/Scripts/message.js
+++ frontend/Scripts/message.js
@@ -3,47 +3,38 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
 
-    var message = document.getElementById("message").value.trim();
-    var senderID = localStorage.getItem("UserID"); // Get sender's ID from localStorage
-    var recipientID = 1; // Fixed recipient ID
+    const message = document.getElementById("message").value.trim();
+    const senderID = localStorage.getItem("UserID");
+    const recipientID = 1;
 
-    // Check if the message is not empty
     if (message === "") {
       alert("Please enter a message.");
       return;
     }
 
-    // Create a FormData object and append data
-    var formData = new FormData();
+    const formData = new FormData();
     formData.append("send_message", "true");
     formData.append("UserID", senderID);
     formData.append("recipientID", recipientID);
     formData.append("message", message);
 
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-
-    // Set up the request
-    xhr.open(
-      "POST",
-      "http://127.0.0.1/Flight-System-Website/backend/message.php",
-      true
-    );
-
-    // Set up a handler for when the request finishes
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        // Request was successful, do something if needed
-        console.log("Message sent:", xhr.responseText);
-        console.log(recipientID);
-        // Optionally, clear the message input after sending
-        document.getElementById("message").value = "";
-      } else {
-        // Request failed, handle error if needed
-        console.error("Failed to send message:", xhr.responseText);
-      }
-    };
-
-    // Send the request with the FormData object as the data
-    xhr.send(formData);
+    try {
+      fetch("http://127.0.0.1/Flight-System-Website/backend/message.php", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((data) => {
+          if (data.status === "error") {
+            console.error("Error updating data:", data.message);
+          } else if (data.status === "success") {
+            console.log(data.message);
+          }
+        });
+    } catch (error) {
+      console.error("Error:", error);
+    }
   });
